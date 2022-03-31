e9b057732a4d7b91255e06b7e6361404cdc266ed
ProphetLamb <prophet.lamb@gmail.com>
2022-03-31 20:10:34 +0200

    Minimize csharp example

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # function-pointer-madness
 How to write function pointer definitions in multiple languages
 
-| Language                       | Source example |
-|--------------------------------|--------------------------------------------|
-| JavaScript                     | https://github.com/Siphalor/function-pointer-madness/blob/c5a348d51851a2220fddc9f583e13f4dbe8bc666/src/main.js#L4-L6 |
-| C#                             | https://github.com/Siphalor/function-pointer-madness/blob/c5a348d51851a2220fddc9f583e13f4dbe8bc666/src/csharp/Program.cs#L9-L14 |
+| Language   | Source example                                                                                                                 |
+| ---------- | ------------------------------------------------------------------------------------------------------------------------------ |
+| JavaScript | https://github.com/Siphalor/function-pointer-madness/blob/c5a348d51851a2220fddc9f583e13f4dbe8bc666/src/main.js#L4-L6           |
+| C#         | https://github.com/Siphalor/function-pointer-madness/blob/c5a348d51851a2220fddc9f583e13f4dbe8bc666/src/csharp/Program.cs#L1-L9 |
--- src/csharp/Program.cs
+++ src/csharp/Program.cs
@@ -1,28 +1,22 @@
-﻿using System;
+﻿// A run of the mill lambda
+Action run = () => Console.WriteLine("Hello World");
 
-namespace FunctionPointerMadness;
-
-public static class Program
+unsafe // Function pointers require unsafe context
 {
-    public static unsafe void Main()
-    {
-        // A run of the mill lambda
-        Action run = () => Console.WriteLine("Hello World");
-        // Function pointers require C# 9 or greater and have virtually no overhead compared to method calls.
-        delegate*<string, void> consume = &Consume;
-        // Static lambdas are much faster, but may not have a closure.
-        Func<string[], string[]> lowercaseAll = strings => strings.Select(static text => text.ToLower()).ToArray();
+    // Function pointers require C# 9 or greater and have virtually no overhead compared to method calls.
+    delegate*<string, void> consume = &Consume;
+    // Static lambdas are much faster, but may not have a closure.
+    Func<string[], string[]> lowercaseAll = static strings => strings.Select(static text => text.ToLower()).ToArray();
 
-        run();
-        consume("CSharp");
-        foreach (string text in lowercaseAll(new[] { "ABC", "DEF" }))
-        {
-            consume(text);
-        }
-    }
-
-    private static void Consume(string text)
+    run();
+    consume("CSharp");
+    foreach (string text in lowercaseAll(new[] { "ABC", "DEF" }))
     {
-        Console.WriteLine($"Hello {text}!");
+        consume(text);
     }
+}
+
+static void Consume(string text)
+{
+    Console.WriteLine($"Hello {text}!");
 }
