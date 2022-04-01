5ae8d64a04106c4758f71fdc7113f7861917ab0d
ProphetLamb <prophet.lamb@gmail.com>
2022-04-01 12:43:26 +0200

    Rust generics in for_each

--- src/rust/src/main.rs
+++ src/rust/src/main.rs
@@ -1,24 +1,24 @@
 fn main() {
     let run: fn() -> () = || println!("Hello World");
-    let consume: fn(&str) -> () = consume;
+    let consume: fn(&String) -> () = consume;
     let lowercase_all: fn(Vec<&str>) -> Vec<String> = |strings: Vec<&str>| {
         strings
             .into_iter()
             .map(|s| s.to_ascii_lowercase())
             .collect()
     };
-    let for_each: fn(Vec<String>, fn(&str)) -> () = for_each;
+    let for_each: fn(Vec<String>, fn(&String)) -> () = for_each;
 
     run();
-    consume("Rust");
+    consume(&"Rust".to_owned());
     for_each(lowercase_all(vec!["ABC", "DEF"]), consume);
 }
 
-fn consume(text: &str) {
+fn consume(text: &String) {
     println!("Hello {}!", text)
 }
 
-fn for_each<F: Fn(&str)>(strings: Vec<String>, fun: F) {
+fn for_each<T, F: Fn(&T)>(strings: Vec<T>, fun: F) {
     for string in strings.iter() {
         fun(string)
     }
