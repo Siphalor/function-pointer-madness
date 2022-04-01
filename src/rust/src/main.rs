55ab39c74db2279f852fe572d61c7e9e103a6354
Siphalor <info@siphalor.de>
2022-04-01 14:09:25 +0200

    Small Rust adjustments

--- src/rust/src/main.rs
+++ src/rust/src/main.rs
@@ -1,24 +1,21 @@
 fn main() {
     let run: fn() -> () = || println!("Hello World");
-    let consume: fn(&String) -> () = consume;
-    let lowercase_all: fn(Vec<&str>) -> Vec<String> = |strings: Vec<&str>| {
-        strings
-            .into_iter()
-            .map(|s| s.to_ascii_lowercase())
-            .collect()
-    };
-    let for_each: fn(Vec<String>, fn(&String)) -> () = for_each;
+    let consume: fn(&String) -> () = |text| println!("Hello {}!", text);
+    let lowercase_all: fn(Vec<&str>) -> Vec<String> = lowercase_all_fn;
+    let for_each: fn(Vec<String>, fn(&String)) -> () = for_each_fn;
 
     run();
     consume(&"Rust".to_owned());
     for_each(lowercase_all(vec!["ABC", "DEF"]), consume);
 }
 
-fn consume(text: &String) {
-    println!("Hello {}!", text)
+fn lowercase_all_fn(strings: Vec<&str>) -> Vec<String> {
+    strings.into_iter()
+        .map(str::to_ascii_lowercase)
+        .collect()
 }
 
-fn for_each<T, F: Fn(&T)>(strings: Vec<T>, fun: F) {
+fn for_each_fn<T, F: Fn(&T)>(strings: Vec<T>, fun: F) {
     for string in strings.iter() {
         fun(string)
     }
