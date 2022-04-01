c01c83973adbf8b9965b1ecc93a7df1290e4c568
ProphetLamb <prophet.lamb@gmail.com>
2022-04-01 18:29:20 +0200

    Csharp use type alias instead of framework types

--- src/csharp/Program.cs
+++ src/csharp/Program.cs
@@ -1,16 +1,16 @@
 ﻿unsafe // Function pointers require unsafe context
 {
     Action run = () => Console.WriteLine("Hello World");
-    Action<String> consume = static (text) => Console.WriteLine($"Hello {text}!"); // Static lambdas are much faster, but may not have a closure.
+    Action<string> consume = static (text) => Console.WriteLine($"Hello {text}!"); // Static lambdas are much faster, but may not have a closure.
     Func<string[], string[]> lowercaseAll = static strings => strings.Select(static text => text.ToLower()).ToArray();
-    delegate*<IEnumerable<String>, Action<String>, void> forEach = ForEach; // Function pointers require C# 9 or greater and have virtually no overhead compared to method calls.
+    delegate*<IEnumerable<string>, Action<string>, void> forEach = &ForEach; // Function pointers require C# 9 or greater and have virtually no overhead compared to method calls.
 
     run();
     consume("CSharp");
     forEach(lowercaseAll(new[] { "ABC", "DEF" }), consume);
-} 
+}
 
-static void ForEach<T>(this IEnumerable<T> sequence, Action<T> fun)
+static void ForEach<T>(IEnumerable<T> sequence, Action<T> fun)
 {
     foreach (T element in sequence)
     {
