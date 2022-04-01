f98f1273c9dd57a84e9f1000113a43a28108bac3
Siphalor <info@siphalor.de>
2022-04-01 11:10:36 +0200

    Add for_each to C

--- src/main.c
+++ src/main.c
@@ -6,20 +6,25 @@
 void run_fn(void);
 void consume_fn(const char *);
 char **lowercase_all_fn(const char*[], size_t);
+void for_each_fn(const char *[], int, void (*)(const char *));
 
 int main() {
 	void (*run)(void) = run_fn;
 	void (*consume)(const char *) = consume_fn;
 	char **(*lowercase_all)(const char *[], size_t) = lowercase_all_fn;
+	void (*for_each)(const char *[], int, void (*)(const char *)) = for_each_fn;
 
 	run();
 	consume("C");
 	const char *strings[] = { "ABC", "DEF" };
 	char **lower_strings = lowercase_all(strings, 2);
 	if (lower_strings != NULL) {
+		for_each(lower_strings, 2, consume);
+
+
+		// Free lower_strings
 		for (size_t i = 0; i < 2; i++) {
 			char *lower_string = lower_strings[i];
-			consume(lower_string);
 			free(lower_string);
 		}
 		free(lower_strings);
@@ -54,4 +59,10 @@ char **lowercase_all_fn(const char *strings[], size_t count) {
 		*(new_strings_iter++) = new_string;
 	}
 	return new_strings;
+}
+
+void for_each_fn(const char *strings[], int count, void (*fun)(const char *)) {
+	for (size_t i = 0; i < count; i++) {
+		fun(*(strings++));
+	}
 }
