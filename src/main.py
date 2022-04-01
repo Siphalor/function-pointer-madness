a7a91f49ab19edcba61d708276b619b122092320
Siphalor <info@siphalor.de>
2022-04-01 11:40:28 +0200

    Add for_each to Python

--- src/main.py
+++ src/main.py
@@ -4,10 +4,14 @@ def main():
     run: Callable[[], None] = lambda: print("Hello World!")
     consume: Callable[[str], None] = lambda text: print(f"Hello {text}!")
     lowercase_all: Callable[[List[str]], str] = lambda strings: map(str.lower, strings)
+    for_each: Callable[[List[str], Callable[[str], None]], None] = for_each_fn
 
     run()
     consume("Python")
-    for s in lowercase_all(["ABC", "DEF"]):
-        consume(s)
+    for_each(lowercase_all(["ABC", "DEF"]), consume);
+
+def for_each_fn(strings: List[str], fun: Callable[[str], None]) -> None:
+    for string in strings:
+        fun(string)
 
 main()
